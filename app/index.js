34761338c58ee7d34d51037e4fdcdc6351dce88f
Luca Bortolussi <luca.bortolussi1992@gmail.com>
2017-05-14 18:05:01 +0200

    model added

--- app/index.js
+++ app/index.js
@@ -1,63 +1,26 @@
-// var THREE = require('three');
+import tree from './model/Tree';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 'white' );
 document.body.appendChild( renderer.domElement );
 
+/*----------OBJ LOADER---------*/
+var loader = new THREE.FBXLoader();
+loader.load( './app/obj/plane.fbx', function( object ) {
+  object.traverse( function ( child ) {
+    console.log( child.name);
+    child.material = new THREE.MeshBasicMaterial( { color: 'green' } );
+    child.material = new THREE.MeshBasicMaterial( { color: 'red' } );
+  });
 
-var loader = new THREE.STLLoader();
-/*--------------------------TREE-----------------------------*/
-loader.load( './app/obj/tree.stl', function ( geometry ) {
-  // var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-  let material = new THREE.MeshBasicMaterial( { color: "green" } );
-  let mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set( 0,0, 0 );
-  // mesh.rotation.set( 0, - Math.PI / 2, 0 );
-  // mesh.scale.set( 0.5, 0.5, 0.5 );
-  // mesh.castShadow = true;
-  // mesh.receiveShadow = true;
-  scene.add( mesh );
-} );
-/*--------------------------TRUNK-----------------------------*/
-loader.load( './app/obj/trunk.stl', function ( geometry ) {
-  // var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-  let material = new THREE.MeshBasicMaterial( { color: 'brown' } );
-  let mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set( 200, 0, 0 );
-  // // mesh.rotation.set( 0, - Math.PI / 2, 0 );
-  // mesh.scale.set( 0.2, 0.2, 0.2 );
-  // mesh.castShadow = true;
-  // mesh.receiveShadow = true;
-  scene.add( mesh );
-} );
-/*--------------------------BASE-----------------------------*/
-loader.load( './app/obj/base.stl', function ( geometry ) {
-  // var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-  let material = new THREE.MeshBasicMaterial( { color: "yellow" } );
-  let mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set( -200,0, 0 );
-  mesh.rotation.set( - Math.PI / 2, 0, 0 );
-  // mesh.scale.set( 0.1, 0.1, 0.1 );
-  // mesh.castShadow = true;
-  // mesh.receiveShadow = true;
-  scene.add( mesh );
-} );
-/*--------------------------FRUIT-----------------------------*/
-loader.load( './app/obj/fruit.stl', function ( geometry ) {
-  // var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-  let material = new THREE.MeshBasicMaterial( { color: "red" } );
-  let mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set( 0,200, 0 );
-  // mesh.rotation.set( 0, - Math.PI / 2, 0 );
-  // mesh.scale.set( .5, .5, .5 );
-  // mesh.castShadow = true;
-  // mesh.receiveShadow = true;
-  scene.add( mesh );
-} );
+  scene.add( object );
+  console.log(object);
+});
 
-camera.position.z = 500;
+camera.position.z = 20;
 
 var controls;
 controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -67,9 +30,6 @@ controls.rotateSpeed = 0.35;
 
 var render = function () {
   requestAnimationFrame( render );
-
-  // geometry.rotation.x += 0.1;
-  // geometry.rotation.y += 0.1;
 
   renderer.render(scene, camera);
 };
