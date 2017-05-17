36084b89edbfc420c7143d802e52df9e45afb030
Luca Bortolussi <luca.bortolussi1992@gmail.com>
2017-05-17 21:28:32 +0200

    ecommerce template

--- app/index.js
+++ app/index.js
@@ -12,19 +12,54 @@ const THREE = require('three');
 window.THREE = THREE;
 require('./lib/OrbitControls');
 
+const portfolioThumb = document.getElementsByClassName('portfolio-thumb');
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const camera0 = new THREE.PerspectiveCamera(75, 300 / 300, 0.1, 1000);
+const camera1 = new THREE.PerspectiveCamera(75, 300 / 300, 0.1, 1000);
+const camera2 = new THREE.PerspectiveCamera(75, 300 / 300, 0.1, 1000);
+const camera3 = new THREE.PerspectiveCamera(75, 300 / 300, 0.1, 1000);
+
+const renderer0 = new THREE.WebGLRenderer();
+const renderer1 = new THREE.WebGLRenderer();
+const renderer2 = new THREE.WebGLRenderer();
+const renderer3 = new THREE.WebGLRenderer();
+
+const controls0 = new THREE.OrbitControls(camera0, renderer0.domElement);
+const controls1 = new THREE.OrbitControls(camera1, renderer1.domElement);
+const controls2 = new THREE.OrbitControls(camera2, renderer2.domElement);
+const controls3 = new THREE.OrbitControls(camera3, renderer2.domElement);
+
 //  CAMERA
-camera.position.z = 20;
+camera0.position.z = 20;
+camera1.position.z = 20;
+camera2.position.z = 20;
+camera3.position.z = 20;
 // RENDERER
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor('white');
-// CONTROLS
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.rotateSpeed = 0.35;
+renderer0.setSize(300, 300);
+renderer0.setClearColor('#A8E6CF');
+renderer1.setSize(300, 300);
+renderer1.setClearColor('#DCEDC1');
+renderer2.setSize(300, 300);
+renderer2.setClearColor('#FFD3B6');
+renderer3.setSize(300, 300);
+renderer3.setClearColor('#FFAAA5');
+// CONTROLS0
+controls0.enableDamping = true;
+controls0.dampingFactor = 0.25;
+controls0.rotateSpeed = 0.35;
+// CONTROLS1
+controls1.enableDamping = true;
+controls1.dampingFactor = 0.25;
+controls1.rotateSpeed = 0.35;
+// CONTROLS2
+controls2.enableDamping = true;
+controls2.dampingFactor = 0.25;
+controls2.rotateSpeed = 0.35;
+// CONTROLS3
+controls3.enableDamping = true;
+controls3.dampingFactor = 0.25;
+controls3.rotateSpeed = 0.35;
 // LIGHT
 const lightMesh = new THREE.Mesh(
   new THREE.SphereGeometry(1, 16, 16),
@@ -35,7 +70,11 @@ const lightMesh = new THREE.Mesh(
 lightMesh.position.set(7.0, 7.0, 7.0);
 scene.add(lightMesh);
 
-document.body.appendChild(renderer.domElement);
+
+portfolioThumb[0].appendChild(renderer0.domElement);
+portfolioThumb[1].appendChild(renderer1.domElement);
+portfolioThumb[2].appendChild(renderer2.domElement);
+portfolioThumb[3].appendChild(renderer3.domElement);
 
 let shaderMaterial = '';
 
@@ -93,7 +132,10 @@ loadTexture('roughnessMap', 'app/textures/wood_mahogany_Roughness.png');
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  renderer0.render(scene, camera0);
+  renderer1.render(scene, camera1);
+  renderer2.render(scene, camera2);
+  renderer3.render(scene, camera3);
 }
 
 render();
