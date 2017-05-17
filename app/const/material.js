c5bda1c9caf7112bea87473fc79a998bb10d0dc0
Luca Bortolussi <Luca Bortolussi>
2017-05-17 14:40:22 +0200

    bndle created

--- app/const/material.js
+++ app/const/material.js
@@ -8,7 +8,7 @@ const material = {
     diffuseMap: { type: 't', value: null },
     roughnessMap: { type: 't', value: null },
     pointLightPosition: { type: 'v3', value: new THREE.Vector3(7, 7, 7) },
-    clight: { type: 'v3', value: new THREE.Vector3(1, 1, 1) },
+    clight: { type: 'v3', value: new THREE.Vector3(1, 0.5, 0.5) },
     textureRepeat: { type: 'v2', value: new THREE.Vector2(1, 1) },
   },
 };
